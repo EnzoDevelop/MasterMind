ad3679f85c2aebf671cab30c38fd0c90fb039bf7
EnzoDevelop <duval.enzo@icloud.com>
2024-10-18 14:18:46 +0200

    Mise en place de la connexion a la bdd

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/mastermind";
+    private static final String USER = "root";
+    private static final String PASSWORD = "FwerTc-XcxK0B8l*";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
