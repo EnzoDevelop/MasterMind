f41ef343ad02fbae82cf31f5a34f4d52f0905c49
Kylian Mahé <kylian.mahe.pro@gmail.com>
2024-10-18 14:38:19 +0200

    classe Joueur

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,3 +1,30 @@
 public class Joueur {
-    private String pseudo;
+    private int idJoueur;
+    private String nom;
+
+    public Joueur(int idJoueur, String nom) {
+        this.idJoueur = idJoueur;
+        this.nom = nom;
+    }
+
+    public int getIdJoueur() {
+        return idJoueur;
+    }
+
+    public void setIdJoueur(int idJoueur) {
+        this.idJoueur = idJoueur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void afficherInfos() {
+        System.out.println("ID Joueur: " + idJoueur);
+        System.out.println("Nom Joueur: " + nom);
+    }
 }
